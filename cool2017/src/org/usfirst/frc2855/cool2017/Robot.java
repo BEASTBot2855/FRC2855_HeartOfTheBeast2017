4e8351aabe62cd58a7ac62571c8795087bceba23
maxnz <mnmax.narvaez3@gmail.com>
2017-03-17 14:16:24 -0500

    Added gyro code

--- cool2017/src/org/usfirst/frc2855/cool2017/Robot.java
+++ cool2017/src/org/usfirst/frc2855/cool2017/Robot.java
@@ -17,6 +17,8 @@ import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+
+import org.usfirst.frc2855.cool2017.SPIGyro.ADXRS453Gyro;
 import org.usfirst.frc2855.cool2017.commands.*;
 import org.usfirst.frc2855.cool2017.subsystems.*;
 
