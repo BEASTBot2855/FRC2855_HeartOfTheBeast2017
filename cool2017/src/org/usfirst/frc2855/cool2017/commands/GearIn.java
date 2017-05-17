1e885b124292aa97fc2f5ec2be6200ff9f2e6c3c
maxnz <mnmax.narvaez3@gmail.com>
2017-05-17 08:42:16 -0500

    Removed unnecessary folder

--- cool2017/src/org/usfirst/frc2855/cool2017/commands/GearIn.java
+++ cool2017/src/org/usfirst/frc2855/cool2017/commands/GearIn.java
@@ -1,11 +1,13 @@
 package org.usfirst.frc2855.cool2017.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 import org.usfirst.frc2855.cool2017.RobotMap;
 import org.usfirst.frc2855.cool2017.Robot;
 
 /**
- *
+ * tells arm solenoid to pull gear arm in
  */
 public class GearIn extends Command {
 
@@ -17,6 +19,7 @@ public class GearIn extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	SmartDashboard.putString("Gear Arm Status", "Retracted");
     }
 
     // Called repeatedly when this Command is scheduled to run
--- cool2017/src/org/usfirst/frc2855/cool2017/commands/GearPinch.java
+++ cool2017/src/org/usfirst/frc2855/cool2017/commands/GearPinch.java
@@ -3,25 +3,27 @@ package org.usfirst.frc2855.cool2017.commands;
 import org.usfirst.frc2855.cool2017.Robot;
 
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
- *
+ * tells gear solenoid to pinch gear
  */
 public class GearPinch extends Command {
 
     public GearPinch() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
-    	requires(Robot.geargrab);
+    	requires(Robot.gearpinch);
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	SmartDashboard.putString("Gear Status", "Pinched");
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.geargrab.gearPinch();
+    	Robot.gearpinch.gearPinch();
     }
 
     // Make this return true when this Command no longer needs to run execute()
@@ -31,7 +33,7 @@ public class GearPinch extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
-    	Robot.geargrab.gearPinchNull();
+    	Robot.gearpinch.gearPinchNull();
     }
 
     // Called when another command which requires one or more of the same
