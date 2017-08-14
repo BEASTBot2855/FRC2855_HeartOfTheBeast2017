7a2719deb9c6c9f111fa572543053ec369d877e7
BEASTBot <BEASTBot@10.193.50.181>
2017-08-14 12:21:58 -0500

    changed movedarobot to drive

--- cool2017/src/org/usfirst/frc2855/cool2017/commands/AutoReverse.java
+++ cool2017/src/org/usfirst/frc2855/cool2017/commands/AutoReverse.java
@@ -23,7 +23,7 @@ public class AutoReverse extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.movedarobot.driveIntake();
+    	Robot.drive.driveIntake();
     }
 
     // Make this return true when this Command no longer needs to run execute()
@@ -33,7 +33,7 @@ public class AutoReverse extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
-    	Robot.movedarobot.stop();
+    	Robot.drive.stop();
     }
 
     // Called when another command which requires one or more of the same
--- cool2017/src/org/usfirst/frc2855/cool2017/commands/AutoTurn.java
+++ cool2017/src/org/usfirst/frc2855/cool2017/commands/AutoTurn.java
@@ -23,7 +23,7 @@ public class AutoTurn extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.movedarobot.turnLeft();
+    	Robot.drive.turnLeft();
     }
 
     // Make this return true when this Command no longer needs to run execute()
@@ -33,7 +33,7 @@ public class AutoTurn extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
-    	Robot.movedarobot.stop();
+    	Robot.drive.stop();
     }
 
     // Called when another command which requires one or more of the same
