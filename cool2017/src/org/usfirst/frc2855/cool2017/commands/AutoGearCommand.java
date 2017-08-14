e1143777f14ec6e718fa827db1b8766071fa8656
BEASTBot <BEASTBot@10.193.50.120>
2017-08-14 12:22:32 -0500

    changed autogear and calgyro

--- cool2017/src/org/usfirst/frc2855/cool2017/commands/AutoGearCommand.java
+++ cool2017/src/org/usfirst/frc2855/cool2017/commands/AutoGearCommand.java
@@ -25,7 +25,7 @@ public class AutoGearCommand extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.movedarobot.driveGear();
+    	Robot.drive.driveGear();
     }
 
     // Make this return true when this Command no longer needs to run execute()
@@ -35,7 +35,7 @@ public class AutoGearCommand extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
-    	Robot.movedarobot.stop();
+    	Robot.drive.stop();
     }
 
     // Called when another command which requires one or more of the same
